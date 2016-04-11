9830800f27d533ec319a467b3ba7e0aff8cdccfd
Tyler Benton <tjbenton21@gmail.com>
2016-04-11 11:48:27 -0400

    Fixed a syntax error
    FlyJS was not transpiling the `...` correctly which was causing errors

--- index.js
+++ index.js
@@ -1,5 +1,9 @@
 module.exports = function() {
-  this.filter("jade", (data, options) => ({
-    css: require("jade").render(data.toString(), {...options, filename: options.base ? `${options.base}/${options.filename}` : options.filename}), ext: ".html"
-  }))
+  this.filter("jade", (data, options) => {
+    options.filename = options.base ? `${options.base}/${options.filename}` : options.filename
+    return {
+      css: require("jade").render(data.toString(), options), 
+      ext: ".html"
+    }
+  })
 }
